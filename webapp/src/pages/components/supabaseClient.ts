1558076c05953d9175d8af85d484b1a4dde03748
Aaron DeVera <aaronsdevera@gmail.com>
2023-03-11 15:10:55 -0500

    non assertion operator

--- webapp/src/pages/components/supabaseClient.ts
+++ webapp/src/pages/components/supabaseClient.ts
@@ -1,7 +1,7 @@
 // supabase.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+let supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!; 
+let supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
