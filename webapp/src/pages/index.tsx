998e95e794010fe5ef924af889be897d85f9afeb
Aaron DeVera <aaronsdevera@gmail.com>
2023-03-12 17:57:37 -0400

    attempt a push as HeadlineEntry

--- webapp/src/pages/index.tsx
+++ webapp/src/pages/index.tsx
@@ -20,9 +20,12 @@ type HeadlineEntry = {
 export async function getServerSideProps() {
   let headlines_array: HeadlineEntry[] = [];
   let { data } = await supabase.from('headlines').select('*').order('created_at', { ascending: false }).limit(250);
+  data?.forEach((entry: HeadlineEntry) => {
+    headlines_array.push(entry);
+  });
   return {
     props: {
-     headlines_array: data
+     "headlines_array": headlines_array
     },
   }
 }
