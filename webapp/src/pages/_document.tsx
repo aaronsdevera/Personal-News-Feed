e4e6bee863a1f10f073864e87a8349ead1a2817c
Aaron DeVera <aaronsdevera@gmail.com>
2023-03-12 20:40:04 -0400

    add the footer back and make a csv

--- webapp/src/pages/_document.tsx
+++ webapp/src/pages/_document.tsx
@@ -9,6 +9,11 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+      <footer>
+          <center>
+            <a href="https://github.com/aaronsdevera/Personal-News-Feed">(haphazardly) designed by Aaron DeVera. GNU-AGPLv3</a>
+          </center>
+      </footer>
     </Html>
   )
 }
