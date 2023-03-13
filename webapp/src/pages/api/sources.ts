4417f122cd71b8c3f4eeeb42080fa5bf604e1866
Aaron DeVera <aaronsdevera@gmail.com>
2023-03-12 20:58:37 -0400

    clean up api and extra files

--- webapp/src/pages/api/sources.ts
+++ webapp/src/pages/api/sources.ts
@@ -9,12 +9,16 @@ let supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
 type Data = {
-  name: string
+  id: string,
+  created_at: string,
+  source_name: string,
+  source_type: string,
+  url: string
 }
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
   let { data } = await supabase.from('sources').select('*').order('created_at', { ascending: false }).limit(250);
   res.status(200).json(data)
