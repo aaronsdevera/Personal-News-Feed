b6a6e485488582d01d30cf2ea66e03abb5dc0d92
Aaron DeVera <aaronsdevera@gmail.com>
2023-08-30 02:52:14 -0400

    update components

--- webapp/src/pages/_document.tsx
+++ webapp/src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
-import { OsJs, OsJsPx } from './onsitejs'
+import { OsJs, OsJsPx } from '../components/onsitejs'
 
 export default function Document() {
   return (
